4fc8360702994b78d3298c896193e0684fc2043f
Sindhuja966 <91117067+Sindhuja966@users.noreply.github.com>
2024-01-17 12:22:02 +0530

    Add files via upload
    commit

---
+++ banking/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "testmod/app"
+
+//import "github.com/Sindhuja966/banking.git/app"
+
+func main() {
+
+	app.Start()
+}
+
+//xml.NewEncoder(w).Encode(customers)
+//json.NewEncoder(w).Encode(customers)
