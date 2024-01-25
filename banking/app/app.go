22bb8b43628558bd8e037c188340a9af5da0a79e
Sindhuja966 <91117067+Sindhuja966@users.noreply.github.com>
2024-01-25 09:36:02 +0530

    Updated the changes  app.go

--- banking/app/app.go
+++ banking/app/app.go
@@ -1,18 +1,35 @@
 package app
 
 import (
-	"log"
-	"net/http"
+	"github.com/Sindhuja966/banking/domain"
+	"github.com/Sindhuja966/banking/service"
+	"github.com/gin-gonic/gin"
 )
 
+func SetupRouter() *gin.Engine {
+
+	//mux := http.NewServeMux()
+	router := gin.Default()
+	// create an instance of the repository
+	customerRepo := domain.NewCustomerRepositoryDb()
+
+	// create an instance of the service and inject the repository
+	customerService := service.NewCustomerService(customerRepo)
+	//unit testcases
+	//customerService := service.NewMockCustomerService(customerRepo)
+
+	// create an instance of the handlers and inject the services
+	customerHandlers := &CustomerHandlers{service: customerService}
+
+	router.GET("/customers", customerHandlers.GetAllCustomers)          // To List all Order
+	router.GET("/customers/:customer_id", customerHandlers.GetCustomer) // To Get Order Request for particular ID
+	//router.POST("/postCustomer", customerHandlers.CreateCustomer)       // To Post Order request
+	return router
+
+}
+
+// starting server
 func Start() {
-
-	mux := http.NewServeMux()
-	//define routes
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
-
-	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8080", mux))
-
+	r := SetupRouter()
+	r.Run("localhost:8080")
 }
