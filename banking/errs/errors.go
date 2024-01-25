c454759bb6ef7b77a08f4c45e3716829862c2e04
Sindhuja966 <91117067+Sindhuja966@users.noreply.github.com>
2024-01-25 12:38:30 +0530

    Update errors.go

--- banking/errs/errors.go
+++ banking/errs/errors.go
@@ -20,3 +20,11 @@ func NewUnexpectedError(message string) *AppError {
 		Code:    http.StatusInternalServerError,
 	}
 }
+
+func NewValidationError(message string) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    http.StatusUnprocessableEntity,
+	}
+}
+
